43d84fdd5ac712c9d824a1374a1e5d5ffb416d92
Everson Soucek <sousekjuniorr@outlook.com>
2023-01-19 17:09:57 -0300

    Começo do site, primeiros contexts feitos

---
+++ README.md
@@ -0,0 +1,1 @@
+Site feito pelo curso da Alura usando react context, uma feira com diversas opções adicionando os itens escolhidos no carrinho.
---
+++ src/pages/Login/index.js
@@ -0,0 +1,60 @@
+import { Button } from '@material-ui/core';
+import {
+	Container,
+	Titulo,
+	InputContainer
+} from './styles';
+import {
+	Input,
+	InputLabel,
+	InputAdornment
+} from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { UsuarioContext } from 'common/context/Usuario';
+import { useContext } from 'react';
+
+function Login() {
+	const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext)
+	const history = useHistory()
+	return (
+		<Container>
+			<Titulo>
+				Insira o seu nome
+			</Titulo>
+			<InputContainer>
+				<InputLabel>
+					Nome
+				</InputLabel>
+				<Input
+					value={nome}
+					type="text"
+					onChange={(event) => setNome(event.target.value)}
+				/>
+			</InputContainer>
+			<InputContainer>
+				<InputLabel>
+					Saldo
+				</InputLabel>
+				<Input
+					type="number"
+					value={saldo}
+					onChange={(event) => setSaldo(event.target.value)}
+					startAdornment={
+						<InputAdornment position="start">
+							R$
+						</InputAdornment>
+					}
+				/>
+			</InputContainer>
+			<Button
+				variant="contained"
+				color="primary"
+				onClick={() => { history.push('/feira') }}
+			>
+				Avançar
+			</Button>
+		</Container>
+	)
+};
+
+export default Login;
