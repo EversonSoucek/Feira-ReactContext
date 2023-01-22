2433634314ebd113ea4a8b9651b0245b4aa20496
Everson Soucek <sousekjuniorr@outlook.com>
2023-01-22 16:14:56 -0300

    Carrinho Completado

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,33 @@
-import Carrinho from 'pages/Carrinho'
-import Login from 'pages/Login'
-import Feira from 'pages/Feira'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { UsuarioProvider } from 'common/context/Usuario'
-import { CarrinhoProvider } from 'common/context/Carrinho'
+import Carrinho from "pages/Carrinho";
+import Feira from "pages/Feira";
+import Login from "pages/Login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { UsuarioProvider } from 'common/context/Usuario';
+import { CarrinhoProvider } from 'common/context/Carrinho';
+import { PagamentoProvider } from "common/context/Pagamento";
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <UsuarioProvider>
-                    <Route exact path='/'><Login /></Route>
-                    <CarrinhoProvider><Route path='/feira'><Feira /></Route></CarrinhoProvider>
-                </UsuarioProvider>
-                <Route path='/carrinho'><Carrinho /></Route>
-            </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Switch>
+        <UsuarioProvider>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <CarrinhoProvider>
+            <Route path="/feira">
+              <Feira />
+            </Route>
+            <PagamentoProvider>
+              <Route path="/carrinho">
+                <Carrinho />
+              </Route>
+            </PagamentoProvider>
+          </CarrinhoProvider>
+        </UsuarioProvider>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default Router
+export default Router;
